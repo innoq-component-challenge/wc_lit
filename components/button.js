ecc69d401230458ef8a575697ab9bb6327378dcf
Joachim Praetorius <joachim.praetorius@innoq.com>
2022-08-25 10:53:46 +0200

    add more functionality
    - dynamic header level in the card
    - passthrough attributes in the button
    - static properties is sufficient, so we don't write a getter

--- components/button.js
+++ components/button.js
@@ -1,5 +1,5 @@
 import 'https://cdnjs.cloudflare.com/ajax/libs/classnames/2.3.1/index.min.js';
-import { BootstrapElement, html } from '../bootstrap_element.js';
+import { BootstrapElement, html, Directive, directive } from '../bootstrap_element.js';
 
 const STYLES = {
 	default: 'btn-secondary',
@@ -8,22 +8,35 @@ const STYLES = {
 
 class LitButton extends BootstrapElement {
 
-	static get properties() {
-		return {
+	static properties = {
+		type: String,
+		additionalClasses: {
 			type: String,
-			additionalClasses: {
-				type: String,
-				attribute: 'additional-classes'
-			}
+			attribute: 'additional-classes'
 		}
 	}
 
 	render() {
 		let cls = classNames('btn', STYLES[this.type] || STYLES.default, this.additionalClasses);
-		return html`<button class=${cls}><slot></slot></button>`;
+		return html`<button ${ attrs() } class=${ cls }><slot></slot></button>`;
 	}
 
 
 }
+
+class AttributePassThrough extends Directive {
+	update(part) {
+		for (const attr of part.options.host.attributes) {
+			// exclude class (since we set that ourselves) and any input properties to the component
+			if (attr.name !== 'class' && LitButton.properties[attr.name] === undefined) {
+				part.element.setAttribute(attr.name, attr.value);
+			}
+		}
+		return '';
+	}
+}
+
+
+const attrs = directive(AttributePassThrough);
 
 export default LitButton.register('lit-button');
